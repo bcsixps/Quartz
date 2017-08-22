78166bf8187612706aae45d34fe11a5c86591f94
bcsixpx <bcsix_px@163.com>
2017-08-22 11:56:51 +0800

    memcached first

---
+++ memcached/src/main/java/com/panshuai/memcached/MemcachedJava.java
@@ -0,0 +1,32 @@
+package com.panshuai.memcached;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Future;
+
+import net.spy.memcached.MemcachedClient;
+
+public class MemcachedJava {
+   public static void main(String[] args) {
+   
+      try{
+         // 连接本地的 Memcached 服务
+         MemcachedClient mcc = new MemcachedClient(new InetSocketAddress("127.0.0.1", 11211));
+         System.out.println("Connection to server sucessful.");
+      
+         // 存储数据
+         Future fo = mcc.set("test", 9, "test old date");  //9秒后过期
+      
+         // 查看存储状态
+         System.out.println("set status:" + fo.get());
+         
+         // 输出值
+         System.out.println("runoob value in cache - " + mcc.get("test"));
+
+         // 关闭连接
+         mcc.shutdown();
+         
+      }catch(Exception ex){
+         System.out.println( ex.getMessage() );
+      }
+   }
+}
